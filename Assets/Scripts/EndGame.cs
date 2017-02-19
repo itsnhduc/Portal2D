3aea7ab4f220d90349b1e05bf44c9a01f2d5d79d
balkanski6 <balkanski_6@abv.bg>
2017-02-19 17:08:57 +0800

    End Level added

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    public GUIText endGame;
+
+	// Use this for initialization
+	void Start () {
+        endGame.text = "";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D coll) {
+        Debug.Log(coll.transform.tag);
+        if (coll.transform.tag == "Player") {
+            endGame.text = "Level Complete. Portals used:" + UnityStandardAssets._2D.Platformer2DUserControl.k;
+        } }
+}
