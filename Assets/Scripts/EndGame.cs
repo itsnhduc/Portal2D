f74619aea545891631d34c58cc1fa7f54d0a22ec
balkanski6 <balkanski_6@abv.bg>
2017-03-06 10:46:56 +0800

    Home buttons added

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour {
 
     public GUIText endGame;
 
+    public Button nextLevel;
+
 	// Use this for initialization
 	void Start () {
         endGame.text = "";
+        nextLevel.interactable = false;
 		
 	}
 	
@@ -20,5 +24,6 @@ public class EndGame : MonoBehaviour {
         Debug.Log(coll.transform.tag);
         if (coll.transform.tag == "Player") {
             endGame.text = "Level Complete. Portals used:" + UnityStandardAssets._2D.Platformer2DUserControl.k;
+            nextLevel.interactable = true;
         } }
 }
--- Assets/Scripts/menuController.cs
+++ Assets/Scripts/menuController.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class menuController : MonoBehaviour {
+
+    
+    void Start () {
+
+    }
 
     public void LoadScene(string scene) {
         SceneManager.LoadScene(scene);
