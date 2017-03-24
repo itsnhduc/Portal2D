7e542d49f827d4121a63636e15e66a4794c72223
Duc Nguyen <nhduc.lhp@gmail.com>
2017-03-24 14:55:13 +0800

    Fix main menu & build settings

---
+++ Assets/Scripts/BackendAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackendAdapter : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/menuController.cs
+++ Assets/Scripts/menuController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class menuController : MonoBehaviour {
+public class MenuController : MonoBehaviour {
 
     
     void Start () {
