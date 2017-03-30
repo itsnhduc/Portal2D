d8e6317863ff6fd205c8f26d912e0bf51ff08808
balkanski6 <balkanski_6@abv.bg>
2017-03-30 17:36:41 +0800

    Username Script

---
+++ Assets/Scripts/UsernameInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsernameInput : MonoBehaviour {
+
+    public InputField input;
+    private bool allowEnter;
+    public Canvas canvas;
+    private SaveScores ss = new SaveScores();
+    void Start()
+    {        
+        input = gameObject.GetComponent<InputField>();
+        canvas = gameObject.GetComponent<Canvas>();
+       
+    }
+
+    void Update() {
+        if (/*input.isFocused*/ allowEnter && input.text != "" && (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter)))
+        {
+            //DoSomething
+            Debug.Log("Update Scores");
+            StartCoroutine(ss.PostScores(input.text, UnityStandardAssets._2D.Platformer2DUserControl.k));
+            input.text = "";
+            allowEnter=false;
+        }
+        else { allowEnter = input.isFocused; }
+    }
+}
